8da49b79c6928314b88f175f01c3539f6af64033
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 20:04:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,19 +15,22 @@ function addElementToEndOfArray(array, element){
   return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array, element){
-  return array.push(element)
+  array.push(element)
+  return array
 }
 function accessElementInArray(array, index){
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift()
+  array.shift()
+  return array
 }
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop()
+  array.pop()
+  return array
 }
 function removeElementFromEndOfArray(array){
   return array.slice(0,array.length - 1)
